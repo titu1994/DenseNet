a7b02f987e3b1edd0b7e2a0c4bd312a552bafba1
Somshubra Majumdar <titu1994@gmail.com>
2016-11-08 17:23:07 +0530

    Initial commit

---
+++ cifar10.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+
+import densenet
+import numpy as np
+import sklearn.metrics as metrics
+
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+batch_size = 64
+nb_classes = 10
+nb_epoch = 10
+
+img_rows, img_cols = 32, 32
+img_channels = 3
+
+img_dim = (img_channels, img_rows, img_cols) if K.image_dim_ordering() == "th" else (img_rows, img_cols, img_channels)
+depth = 40
+nb_dense_block = 3
+growth_rate = 12
+nb_filter = 16
+dropout_rate = 0.2
+
+model = densenet.DenseNet(nb_classes, img_dim, depth, nb_dense_block, growth_rate, nb_filter,
+                                  dropout_rate=dropout_rate)
+print("Model created")
+
+model.summary()
+optimizer = Adam()
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
+print("Finished compiling")
+print("Building model...")
+
+(trainX, trainY), (testX, testY) = cifar10.load_data()
+
+Y_train = np_utils.to_categorical(trainY, nb_classes)
+Y_test = np_utils.to_categorical(testY, nb_classes)
+
+generator = ImageDataGenerator(rotation_range=15,
+                               width_shift_range=5./32,
+                               height_shift_range=5./32)
+
+generator.fit(trainX, seed=0, augment=True)
+
+# Load model
+model.load_weights("weights/DenseNet-40-12-CIFAR10.h5")
+print("Model loaded.")
+
+model.fit_generator(generator.flow(trainX, Y_train, batch_size=batch_size), samples_per_epoch=len(trainX), nb_epoch=nb_epoch,
+                   callbacks=[ModelCheckpoint("weights/DenseNet-40-12-CIFAR10.h5", monitor="val_acc", save_best_only=True,
+                                              save_weights_only=True)],
+                   validation_data=(testX, Y_test),
+                   nb_val_samples=testX.shape[0],)
+
+yPreds = model.predict(testX)
+yPred = np.argmax(yPreds, axis=1)
+yTrue = testY
+
+accuracy = metrics.accuracy_score(yTrue, yPred) * 100
+error = 100 - accuracy
+print("Accuracy : ", accuracy)
+print("Error : ", error)
+
---
+++ densenet.py
@@ -0,0 +1,135 @@
+from keras.models import Model
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import AveragePooling2D
+from keras.layers.pooling import GlobalAveragePooling2D
+from keras.layers import Input, merge
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+import keras.backend as K
+
+concat_axis = 1 if K.image_dim_ordering() == "th" else -1
+
+def conv_block(ip, nb_filter, dropout_rate=None, weight_decay=1E-4):
+    ''' Apply BatchNorm, Relu 3x3Conv2D, optional dropout
+
+    Args:
+        ip: Input keras tensor
+        nb_filter: number of filters
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+
+    Returns: keras tensor with batch_norm, relu and convolution2d added
+
+    '''
+
+    x = BatchNormalization(mode=0, axis=concat_axis, gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(ip)
+    x = Activation('relu')(x)
+    x = Convolution2D(nb_filter, 3, 3, init="he_uniform", border_mode="same", ias=False,
+                      W_regularizer=l2(weight_decay))(x)
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    return x
+
+
+def transition(ip, nb_filter, dropout_rate=None, weight_decay=1E-4):
+    ''' Apply BatchNorm, Relu 1x1Conv2D, optional dropout and Maxpooling2D
+
+    Args:
+        ip: keras tensor
+        nb_filter: number of filters
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+
+    Returns: keras tensor, after applying batch_norm, relu-conv, dropout, maxpool
+
+    '''
+
+    x = BatchNormalization(mode=0, axis=concat_axis, gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(ip)
+    x = Activation('relu')(x)
+    x = Convolution2D(nb_filter, 1, 1, init="he_uniform", border_mode="same", bias=False,
+                      W_regularizer=l2(weight_decay))(x)
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+    x = AveragePooling2D((2, 2), strides=(2, 2))(x)
+
+    return x
+
+
+def denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=None, weight_decay=1E-4):
+    ''' Build a denseblock where the output of each conv_block is fed to subsequent ones
+
+    Args:
+        x: keras tensor
+        nb_layers: the number of layers of conv_block to append to the model.
+        nb_filter: number of filters
+        growth_rate: growth rate
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+
+    Returns: keras tensor with nb_layers of conv_block appended
+
+    '''
+
+    feature_list = [x]
+
+    for i in range(nb_layers):
+        x = conv_block(x, growth_rate, dropout_rate, weight_decay)
+        feature_list.append(x)
+        x = merge(feature_list, mode='concat', concat_axis=concat_axis)
+        nb_filter += growth_rate
+
+    return x, nb_filter
+
+
+def DenseNet(nb_classes, img_dim, depth, nb_dense_block, growth_rate, nb_filter, dropout_rate=None, weight_decay=1E-4):
+    ''' Build the DenseNet model
+
+    Args:
+        nb_classes: number of classes
+        img_dim: tuple of shape (channels, rows, columns) or (rows, columns, channels)
+        depth: number or layers
+        nb_dense_block: number of dense blocks to add to end
+        growth_rate: number of filters to add
+        nb_filter: number of filters
+        dropout_rate: dropout rate
+        weight_decay: weight decay
+
+    Returns: keras tensor with nb_layers of conv_block appended
+
+    '''
+
+    model_input = Input(shape=img_dim)
+
+    assert (depth - 4) % 3 == 0, "Depth must be 3 N + 4"
+
+    # layers in each dense block
+    nb_layers = int((depth - 4) / 3)
+
+    # Initial convolution
+    x = Convolution2D(nb_filter, 3, 3, init="he_uniform", border_mode="same", name="initial_conv2D", bias=False,
+                      W_regularizer=l2(weight_decay))(model_input)
+
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        x, nb_filter = denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate,
+                                  weight_decay=weight_decay)
+        # add transition
+        x = transition(x, nb_filter, dropout_rate=dropout_rate, weight_decay=weight_decay)
+
+    # The last denseblock does not have a transition
+    x, nb_filter = denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate,
+                              weight_decay=weight_decay)
+
+    x = BatchNormalization(mode=0, axis=concat_axis, gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(x)
+    x = Activation('relu')(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(nb_classes, activation='softmax', W_regularizer=l2(weight_decay), b_regularizer=l2(weight_decay))(x)
+
+    densenet = Model(input=model_input, output=x, name="DenseNet")
+
+    return densenet
