da910658f503e7508ecc40fb63dd6461a5c5fff4
Somshubra Majumdar <titu1994@gmail.com>
2017-11-29 23:18:06 -0600

    Fix https://github.com/titu1994/DenseNet/issues/33

--- tensorflow_backend.py
+++ tensorflow_backend.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 
 from keras.backend import tensorflow_backend as KTF
 from keras.backend.common import image_data_format
-from keras.backend.tensorflow_backend import _preprocess_conv2d_input
-from keras.backend.tensorflow_backend import _postprocess_conv2d_output
 
 py_all = all
 
@@ -11,8 +9,12 @@ def depth_to_space(input, scale, data_format=None):
     ''' Uses phase shift algorithm to convert channels/depth for spatial resolution '''
     if data_format is None:
         data_format = image_data_format()
+
+    if data_format == 'channels_first':
+        data_format = 'NCHW'
+    else:
+        data_format = 'NHWC'
+
     data_format = data_format.lower()
-    input = _preprocess_conv2d_input(input, data_format)
-    out = tf.depth_to_space(input, scale)
-    out = _postprocess_conv2d_output(out, data_format)
+    out = tf.depth_to_space(input, scale, data_format=data_format)
     return out
